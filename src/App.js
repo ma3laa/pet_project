a7ff6e596e19cb5068b0748534a56fc081380879
ma3laa <lukametolli@gmail.com>
2025-03-10 18:58:45 +0000

    route&format

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(<Route path="/" element={ <Root/> }></Route>))
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={ <Root/> }>
+    <Route index element={ <HomePage /> } />
+  </Route>
+))
 
 function App() {
   return (
