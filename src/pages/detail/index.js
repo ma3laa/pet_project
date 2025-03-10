cf88c72471568d66797fd1b262cfe1ecdc76cebc
ma3laa <lukametolli@gmail.com>
2025-03-10 23:32:40 +0000

    finish react routing project

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
-import { useParams } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 
 const PetDetailsPage = () => {
   const [data, setData] = useState();
@@ -31,7 +30,8 @@ const PetDetailsPage = () => {
         <h3>Loading...</h3>
       ) : error ? (
         <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
+          {}
+          <Navigate to="/pet-details-not-found"/>
         </div>
       ) : (
         <main>
