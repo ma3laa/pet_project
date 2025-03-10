08102cfdcf4cdd5c8f0f360f07bd3084d71eeeb9
ma3laa <lukametolli@gmail.com>
2025-03-10 18:56:10 +0000

    first route commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = <Route path="/" element={ <Root/> }></Route>;
+const appRouter = createBrowserRouter(createRoutesFromElements(<Route path="/" element={ <Root/> }></Route>))
 
 function App() {
   return (
